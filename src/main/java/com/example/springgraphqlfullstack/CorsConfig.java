51ab8b5311543ec255f20f7ecbc6dbacaf12ab99
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-06-12 23:00:30 +0200

    Initial commit

---
+++ src/main/java/com/example/springgraphqlfullstack/CorsConfig.java
@@ -0,0 +1,2 @@
+package com.example.springgraphqlfullstack;public class CorsConfig {
+}
---
+++ src/test/java/com/example/springgraphqlfullstack/SpringGraphqlFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgraphqlfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

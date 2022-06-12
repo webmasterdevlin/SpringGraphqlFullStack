0ba11952789f307adbda0f2eca306300f3b9869f
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-06-12 23:16:57 +0200

    update

--- src/main/java/com/example/springgraphqlfullstack/CorsConfig.java
+++ src/main/java/com/example/springgraphqlfullstack/CorsConfig.java
@@ -1,2 +1,56 @@
-package com.example.springgraphqlfullstack;public class CorsConfig {
+package com.example.springgraphqlfullstack;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfiguration.setAllowedHeaders(
+                Arrays.asList(
+                        "Origin",
+                        "Access-Control-Allow-Origin",
+                        "Content-Type",
+                        "Accept",
+                        "Authorization",
+                        "Origin, Accept",
+                        "X-Requested-With",
+                        "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers"
+                )
+        );
+        corsConfiguration.setExposedHeaders(
+                Arrays.asList(
+                        "Origin",
+                        "Content-Type",
+                        "Accept",
+                        "Authorization",
+                        "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Credentials"
+                )
+        );
+        corsConfiguration.setAllowedMethods(
+                Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        );
+        var urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration(
+                "/**",
+                corsConfiguration
+        );
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 }
